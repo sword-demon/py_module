298443607c355d14ba82c9023c285f93f35ab840
wxvirus <1418667580@qq.com>
2021-07-06 00:48:17 +0800

    task对象

--- asyncio高性能异步编程/协程/task对象.py
+++ asyncio高性能异步编程/协程/task对象.py
@@ -11,18 +11,20 @@ async def func():
 async def main():
     print("main开始")
 
-    task1 = asyncio.create_task(func())
-
-    task2 = asyncio.create_task(func())
+    task_list = [
+        asyncio.create_task(func(), name="n1"),  # 起名
+        asyncio.create_task(func(), name="n2")
+    ]
 
     print("main结束")
 
     # 当执行某协程时遇到IO操作，会自动切换其他任务
     # 此处的await是等待相对应的协程全部执行完毕并获取返回结果
-    ret1 = await task1
-    ret2 = await task2
+    # ret1 = await task1
+    # ret2 = await task2
 
-    print(ret1, ret2)
+    done, pending = await asyncio.wait(task_list, timeout=2)  # 最多等2秒
+    print(done, pending)
 
 
 asyncio.run(main())
